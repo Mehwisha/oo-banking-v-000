e6068d47be4b2505f8aac7837659529b7f76e507
Mehwish Aziz <mehwisha@msn.com>
2018-08-10 00:19:48 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,18 +1,18 @@
 class Transfer
 
   attr_accessor :sender, :receiver, :amount, :status
-  
+
     def initialize(sender, receiver, amount)
       @sender = sender
       @receiver = receiver
       @amount = amount
       @status = "pending"
     end
-  
+
     def valid?
       @sender.valid? && @receiver.valid? ? true : false
     end
-  
+
     def execute_transaction
       if @sender.balance > @amount && @status == "pending"
         @sender.balance -= @amount
@@ -23,7 +23,7 @@ class Transfer
         return "Transaction rejected. Please check your account balance."
       end
     end
-  
+
     def reverse_transfer
       if @status == "complete"
         @sender.balance += @amount
